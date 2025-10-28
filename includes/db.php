e1f44085d2d5c28c865ac587c5415182d0df1bb6
Harshil Prajapti <hp5896958@gmail.com>
2025-10-28 23:10:18 +0530

    .

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'student_feedback';
+$servername = "sql204.infinityfree.com";  
+$username   = "if0_40277481";            
+$password   = "WkzZ5PS8fdfaB";           
+$database   = "if0_40277481_student_feedback";  
 
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
